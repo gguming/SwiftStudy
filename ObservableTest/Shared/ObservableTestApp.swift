aa300c18939f9724fb5a598e9d028a86af2b9a1a
HuiseonJang <xkzl8521@gmail.com>
2022-01-06 09:36:30 +0900

    add project

---
+++ ObservableTest/Shared/ObservableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableTestApp.swift
+//  Shared
+//
+//  Created by 장희선 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct ObservableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
