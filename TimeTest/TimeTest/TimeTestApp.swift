60d5782dddcad1c1369c7a1505469a992af23dde
HuiseonJang <xkzl8521@gmail.com>
2022-01-17 17:28:05 +0900

    timestamp to date test

---
+++ TimeTest/TimeTest/TimeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTestApp.swift
+//  TimeTest
+//
+//  Created by 장희선 on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct TimeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
