5f4f0e1224210b10bb0cab94f393f1dadba0b4fb
HuiseonJang <xkzl8521@gmail.com>
2022-01-24 18:30:08 +0900

    rxswift를 사용하여 사진 전달

--- FilteringPhoto/FilteringPhoto/PhotoCollectionViewController.swift
+++ FilteringPhoto/FilteringPhoto/PhotoCollectionViewController.swift
@@ -8,9 +8,13 @@
 import Foundation
 import UIKit
 import Photos
+import RxSwift
 
 class PhotoCollectionViewController: UICollectionViewController {
-    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
     private var images = [PHAsset]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +27,23 @@ class PhotoCollectionViewController: UICollectionViewController {
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            guard let info = info else {return}
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
