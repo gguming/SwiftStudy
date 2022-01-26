25703ad34faf78c7fb22d6c32a4b59b3387a3236
HuiseonJang <xkzl8521@gmail.com>
2022-01-26 15:18:36 +0900

    rx의 커스텀 메소드 추가 및 연결

--- CVS/CVS/Presentation/LocationInformationViewModel.swift
+++ CVS/CVS/Presentation/LocationInformationViewModel.swift
@@ -12,10 +12,16 @@ import RxCocoa
 struct LocationInformationViewModel {
     let disposeBag = DisposeBag()
 
+    // subviewModels
+    let detailListBackgroundViewModel = DetailListBackgroundViewModel()
+    
     
     //viewModel -> view
     let setMapCenter: Signal<MTMapPoint>
     let errorMessage: Signal<String>
+    
+    let detailListCellData: Driver<[DetailListCellData]>
+    let scrollToSelectedLocation: Signal<Int>
     
     //view -> viewModel
     let currentLocation = PublishRelay<MTMapPoint>()
@@ -24,12 +30,31 @@ struct LocationInformationViewModel {
     let mapViewError = PublishRelay<String>()
     let currentLocationButtonTapped = PublishRelay<Void>()
     
+    let detailListItemSelected = PublishRelay<Int>()
+    
+    let documentData = PublishSubject<[KLDocument?]>()
+    
     init() {
         //MARK: 지도 중심점 설정
+        let selectedDetailListItem = detailListItemSelected
+            .withLatestFrom(documentData) { $1[$0] }
+            .map { data -> MTMapPoint in
+                guard let data = data,
+                      let longtitude = Double(data.x),
+                      let latitude = Double(data.y) else {
+                          return MTMapPoint()
+                      }
+                
+                let geoCoord = MTMapPointGeo(latitude: latitude, longitude: longtitude)
+                return MTMapPoint(geoCoord: geoCoord)
+            }
+        
+        
         let moveToCurrentLocation = currentLocationButtonTapped
             .withLatestFrom(currentLocation)
         let currentMapCenter = Observable
             .merge(
+                selectedDetailListItem,
                 currentLocation.take(1),
                 moveToCurrentLocation
             )
@@ -39,6 +64,12 @@ struct LocationInformationViewModel {
         
         errorMessage = mapViewError.asObservable()
             .asSignal(onErrorJustReturn: "잠시 후 다시 시도해 주세요.")
+        
+        detailListCellData = Driver.just([])
+        
+        scrollToSelectedLocation = selectPOIItem
+            .map { $0.tag }
+            .asSignal(onErrorJustReturn: 0)
     }
     
 }
