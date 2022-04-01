e6b156cc092f9aaf5c4ca63d2cf60c960e92b41e
HuiseonJang <xkzl8521@gmail.com>
2022-04-01 17:39:00 +0900

    UIkit tabbar test

---
+++ TabbarTest/TabbarTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbarTest
+//
+//  Created by 장희선 on 2022/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
