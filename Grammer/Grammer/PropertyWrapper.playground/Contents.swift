9d08875fad2bdfc51c4e5bdca7bcfae272f22d23
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-05-10 00:12:47 +0900

    no message

---
+++ Grammer/Grammer/PropertyWrapper.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+@propertyWrapper
+struct TwelveOrLess {
+    private var number = 0
+    var wrapperValue: Int {
+        get { return number }
+        set { number = min(newValue, 12) }
+    }
+}
