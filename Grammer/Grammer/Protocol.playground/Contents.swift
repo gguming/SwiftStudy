2f408aa911ab205dcb613ce7a5204eaa3b7cfed7
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-06-05 17:00:53 +0900

    프로토콜

---
+++ Grammer/Grammer/Protocol.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+protocol FirstProtocol {
+    
+}
+
+protocol AnotherProtocol {
+    
+}
+
+class SomeSuperClass {
+    
+}
+
+
+class SomeClass: SomeSuperClass, FirstProtocol, AnotherProtocol {
+    
+}
