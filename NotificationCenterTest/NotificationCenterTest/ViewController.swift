62208a460cf9b251608f2578ae1a49b00097f0d3
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-06-19 15:58:38 +0900

    notificationcenter 테스트 프로젝트 추가

---
+++ NotificationCenterTest/NotificationCenterTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationCenterTest
+//
+//  Created by SUN on 2022/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
