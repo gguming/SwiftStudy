c5d12bd8bfa56773b73e798802edbd354544213f
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-06-25 22:08:53 +0900

    고차함수 filter, reduce 예제 추가

--- Grammer/Grammer/HighOrderFunction.playground/Contents.swift
+++ Grammer/Grammer/HighOrderFunction.playground/Contents.swift
@@ -5,5 +5,36 @@ let neoguri = "neoguri"
 let neguriWithMap = neoguri.map { cha in
     return cha
 }
+
+var charArr: [Character] = []
+for cha in neoguri {
+    charArr.append(cha)
+}
+
 print(neoguri)
 print(neguriWithMap)
+print(charArr)
+
+
+// MARK: filter
+
+let numberList = [1,2,3,4,5,6,7,8]
+let evenNumberList = numberList.filter { $0%2 == 0 }
+// [2,4,6,8]
+
+
+print(evenNumberList)
+
+
+// MARK: reduce
+
+// 첫번째 메소드 사용
+[1,2,3,4,5].reduce(0) { result, next in
+    return result + next
+}
+
+
+// 두번째 메소드 사용
+[1,2,3,4,5].reduce(into: 0) { result, next in
+    return result += next
+}
