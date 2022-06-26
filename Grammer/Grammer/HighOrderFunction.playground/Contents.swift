d762fc3bc533c6f9a9e73c3476531712d42152ca
HuiSeon Jang <72551674+gguming@users.noreply.github.com>
2022-06-26 17:44:11 +0900

    flatmap compactmap 예제코드 업로드

--- Grammer/Grammer/HighOrderFunction.playground/Contents.swift
+++ Grammer/Grammer/HighOrderFunction.playground/Contents.swift
@@ -11,9 +11,9 @@ for cha in neoguri {
     charArr.append(cha)
 }
 
-print(neoguri)
-print(neguriWithMap)
-print(charArr)
+//print(neoguri)
+//print(neguriWithMap)
+//print(charArr)
 
 
 // MARK: filter
@@ -23,7 +23,7 @@ let evenNumberList = numberList.filter { $0%2 == 0 }
 // [2,4,6,8]
 
 
-print(evenNumberList)
+//print(evenNumberList)
 
 
 // MARK: reduce
@@ -38,3 +38,25 @@ print(evenNumberList)
 [1,2,3,4,5].reduce(into: 0) { result, next in
     return result += next
 }
+
+
+
+[1,2,3,4,5].flatMap { $0 + 2 }
+
+let arr: [[Int]] = [[1], [23, 4], [1,2,3]]
+
+let arrWithFlatMap = arr.flatMap { $0 }
+//print(arrWithFlatMap)
+
+
+let flatMapTestArr = [1,2,3,4,5].compactMap{ $0 }
+
+
+let intArrWithNil: [Int?] = [1,2,nil,5,6,nil]
+//print(intArrWithNil)
+//print(intArrWithNil.compactMap{ $0 })
+
+
+let testArr: [[Int?]] = [[1, nil], [23, 4], [nil,1,2,3]]
+print(testArr.flatMap {$0})
+print(testArr.compactMap {$0})
